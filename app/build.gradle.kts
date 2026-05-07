736b52e077cce549cd6a798110ed7d7685189c78
EternityQwQ <EternityQwQ@users.noreply.github.com>
2026-05-07 09:01:03 +0000

    Add V2 signing key and configure release signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,15 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("thermal_faker_key.jks")
+            storePassword = "thermalfaker"
+            keyAlias = "thermal_faker_key"
+            keyPassword = "thermalfaker"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -29,6 +38,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
